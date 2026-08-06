acca350e0986d97fe5a305f775af8d4bec4d22db
dardyco <hellodardy@gmail.com>
2026-08-06 09:52:04 +0200

    Update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ export default defineConfig({
   plugins: [react()],
   // IMPORTANT: change this to match your GitHub repo name, e.g. '/dardy-site/'
   // If deploying to a custom domain or a user/org page (username.github.io), use '/'
-  base: '/dardy-site/',
+
+  base: '/dardy-website/',
+
+  
 })
